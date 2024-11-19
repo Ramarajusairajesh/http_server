f55ac036b9a46f084a62d4a702e92ea365d9a9d7
Ramarajusairajesh <sairajeshramraju@gmail.com>
2024-11-19 14:29:34 +0530

    http-python3 server-1

--- python3/server.py
+++ python3/server.py
@@ -10,7 +10,8 @@ path = "/home/kubuntu/Documents/low_levle_things/http_server/python3/browny-v1.0
 
 def logging(user_address, network_activity):
     with open(file_name, "a+") as file:
-        file.write(f"{str(datetime.datetime.now())}: {user_address}/{network_activity}\n")
+        file.write(f"{str(datetime.datetime.now())}: {
+                   user_address}/{network_activity}\n")
 
 
 def http_server(server_socket):
@@ -22,13 +23,17 @@ def http_server(server_socket):
     full_path = os.path.abspath(os.path.join(path, request))
     if not full_path.startswith(path) or not os.path.exists(full_path):
         with open(os.path.join(path, "404.html"), "rb") as file:
-            client_socket.sendall(b"HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n")
+            client_socket.sendall(
+                b"HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n"
+            )
             client_socket.sendall(file.read())
     else:
         content_type, _ = mimetypes.guess_type(full_path)
         content_type = content_type or "application/octet-stream"
         with open(full_path, "rb") as file:
-            client_socket.sendall(f"HTTP/1.1 200 OK\r\nContent-Type: {content_type}\r\n\r\n".encode())
+            client_socket.sendall(
+                f"HTTP/1.1 200 OK\r\nContent-Type: {content_type}\r\n\r\n".encode()
+            )
             client_socket.sendall(file.read())
     client_socket.close()
 
@@ -40,4 +45,3 @@ print("The server is running on port", port)
 
 while True:
     http_server(server_socket)
-ind
